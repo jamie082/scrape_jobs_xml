061642b5cc637a19aa86fb0c17aeace83a5f13de
jamie082 <jamiemorrissey02@gmail.com>
2022-05-21 13:57:40 -0700

    added for counter and description

--- scrape_jobs_xml.py
+++ scrape_jobs_xml.py
@@ -8,17 +8,24 @@ response = requests.get("http://sandiego.craigslist.org/search/sof")
 webpage = response.content
 soup = BeautifulSoup(webpage, "html.parser")
 
-updated = [1,2,3,4,5]
 """
 updated=[]
 description=[]
 other=[]
 """
 
-data = []
-data = pd.DataFrame({"updated", updated, "Description", description, "other", other})
+def start_program():
+    for counter in soup.find_all('section', class_='page-container'):
+        for n, tag in enumerate(counter.find_all('li', class_='result-row')):
+            description = [x for x in tag.find_all('li')]
+            print("Description: ", description[n].text.strip())
+            print()
+
+
+data = pd.DataFrame({"Description", description})
 print(pd)
 
 with open('data_write.xml', 'w') as f: # Writing in XML file
     for line in xml_data:
         f.write(line)
+start_program()
