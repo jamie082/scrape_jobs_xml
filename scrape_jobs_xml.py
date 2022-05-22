a7d4e7dc20ee3c4f0d3942a530962041267d146f
jamie082 <jamiemorrissey02@gmail.com>
2022-05-21 19:41:36 -0700

    fixed file

--- scrape_jobs_xml.py
+++ scrape_jobs_xml.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+# craigslist page, data for Python3 http://sandiego.craigslist.org/search/sof
 
 response = requests.get("http://sandiego.craigslist.org/search/sof")
 webpage = response.content
@@ -15,17 +16,20 @@ other=[]
 """
 
 def start_program():
-    for counter in soup.find_all('section', class_='page-container'):
-        for n, tag in enumerate(counter.find_all('li', class_='result-row')):
+    for counter in soup.find_all('form', class_='search-form'):
+        for n, tag in enumerate(counter.find_all('ul', class_='rows')):
             description = [x for x in tag.find_all('li')]
+            # output job
             print("Description: ", description[n].text.strip())
             print()
 
 
+"""
 data = pd.DataFrame({"Description", description})
 print(pd)
 
 with open('data_write.xml', 'w') as f: # Writing in XML file
     for line in xml_data:
         f.write(line)
+        """
 start_program()
